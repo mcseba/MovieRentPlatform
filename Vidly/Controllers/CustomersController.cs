dc6ff6d9b3ee2e24c2c0e5290162ceba1a17e8b9
mcseba <48526736+mcseba@users.noreply.github.com>
2020-11-08 15:49:38 +0100

    Pass ViewModel to the View in Controller

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -26,8 +27,12 @@ namespace Vidly.Controllers
         public IActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
 
-            return View();
+            return View(viewModel);
         }
 
         public IActionResult Index()
