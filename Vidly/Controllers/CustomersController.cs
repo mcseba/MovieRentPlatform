4ac0fb3e1524bee266359bf08f42ef0a10c07c27
mcseba <48526736+mcseba@users.noreply.github.com>
2020-11-17 16:21:59 +0100

    new IActionResult method for creating and saving user in the database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -35,6 +35,15 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
         public IActionResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList().AsQueryable();
