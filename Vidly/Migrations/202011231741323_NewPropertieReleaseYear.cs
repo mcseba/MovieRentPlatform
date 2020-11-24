cc6d2ce78fa0ebc15b87d13ade42298e10810125
mcseba <48526736+mcseba@users.noreply.github.com>
2020-11-24 16:40:53 +0100

    new migration for adding propertie ReleaseYear in Movie Model

---
+++ Vidly/Migrations/202011231741323_NewPropertieReleaseYear.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewPropertieReleaseYear : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "ReleaseYear", c => c.Int(nullable: false));
+            DropColumn("dbo.Movies", "ReleaseDate");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime(nullable: false));
+            DropColumn("dbo.Movies", "ReleaseYear");
+        }
+    }
+}
